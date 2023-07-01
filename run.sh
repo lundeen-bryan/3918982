5945365a49430fc67a100437216e49cf2f9ff09d
lundeen-bryan <lundeen-bryan@gmail.com>
2023-07-01 09:37:16 -0700

    🐞 fix(venv): Removed old venv and requirements file blank, changed run.sh for new environ

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ trap 'kill $PID; exit 1' INT
 echo "Creating virtual environment. Please wait..."
 
 # Create a Python virtual environment with Python 3.7 in the background
-/c/python38/python.exe -m venv dashboard_venv &
+/c/python38/python.exe -m venv .venv &
 PID=$!
 
 # Wait for the virtual environment to be created
@@ -18,16 +18,16 @@ wait $PID
 
 # Add the hidden attribute to the dashboard_venv directory
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    SetFile -a V dashboard_venv
+    SetFile -a V .venv
 else
-    attrib +h dashboard_venv
+    attrib +h .venv
 fi
 
 # Wait for the user to press any key before continuing
 read -p "Virtual environment created. Press Enter to activate and install dependencies..."
 
 # Activate the Python virtual environment
-source dashboard_venv/Scripts/activate
+source .venv/Scripts/activate
 
 # Upgrade pip
 /c/python38/python.exe -m pip install --upgrade pip
